a43b31cec420525deb2de391cee7b0ecb8d493fa
Tigran_K <tigran.kocharyan@outlook.com>
2019-11-26 15:00:34 +0300

    Draw Function + List

---
+++ KDZ/Drawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDZ
+{
+    public class Drawer
+        //This class is supposed to make the CLI perfomance much better by changing the color of messages.
+    {
+        static public void RedConsole(string message)
+        // The function takes the message to the CLI and makes it Red for a good perfomance.
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static public void GreenConsole(string message)
+        // The function takes the message to the CLI and makes it Green for a good perfomance.
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static public void MagentaConsole(string message)
+        // The function takes the message to the CLI and makes it Magenta for a good perfomance.
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- KDZ/Program.cs
+++ KDZ/Program.cs
@@ -9,9 +9,28 @@ namespace KDZ
 {
     class Program
     {
+        static public int ReadInt(string message, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            int number;
+            do
+            {
+                Drawer.GreenConsole(message);
+            } while (!int.TryParse(Console.ReadLine(), out number) || number < minValue || number > maxValue);
+            return number;
+        }
+
         static void Main(string[] args)
         {
+            Drawer.MagentaConsole("\t\t\t\t\t<<НАЧАЛО ТВОЕГО ВЕЛИКОГО ПУТИ>>" + Environment.NewLine);
+            do
+            {
+                int number = ReadInt("Введите количество монет для покупки оружия: ", 1, 100);
+                List<Gun> numbers = new List<Gun>();
 
+
+
+                Drawer.RedConsole("Введите ENTER для продолжения, в противном случае, нажмите на любую клавишу");
+            } while (Console.ReadKey(true).Key == ConsoleKey.Enter);
         }
     }
 }
